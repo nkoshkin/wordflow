3d26f2c20de00f051dfe6da2bc6dc54b262c547f
Nikolay Koshkin <koshkin.nickolay@gmail.com>
2026-03-31 23:05:24 +0400

    init project

---
+++ src/main/java/io/ylab/wordflow/WordflowApplication.java
@@ -0,0 +1,13 @@
+package io.ylab.wordflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/ylab/wordflow/WordflowApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ylab.wordflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
